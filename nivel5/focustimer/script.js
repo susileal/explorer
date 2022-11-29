1d79b7182b7ab5b16034377962e3eb84d325de7c
Susi Leal <susi_leal@hotmail.com>
2022-11-29 10:55:31 -0300

    refatoração utilizando funções

--- nivel5/focustimer/script.js
+++ nivel5/focustimer/script.js
@@ -4,66 +4,77 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonIncrease = document.querySelector('.buttonincrease')
 const buttonDecrease = document.querySelector('.buttondecrease')
-let minutes
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
+let minutes = Number(minutesDisplay.textContent)
+let timerTimeOut
 
-function cauntdow(){
+/* refatoração utilizando funções*/
+function resetControls(){
+  buttonPlay.classList.remove('hide')
+  buttonPause.classList.add('hide')
+  buttonSet.classList.remove('hide')
+  buttonStop.classList.add('hide')
+ 
+}
 
-   /* 1. setTimeout - será adcionado um tempo, depois de um tempo ele irá 
-  executar uma função de callback 
-  2. Primeiro vem a função, depois o tempo que será executado a função
-  */
-  setTimeout(function() {
+function updateTimerDisplay(minutes, seconds){
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+
+}
+
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
+
+function countdow(){
+
+  timerTimeOut = setTimeout(function() {
     let seconds = Number(secondsDisplay.textContent)
     let minutes = Number(minutesDisplay.textContent)
 
-      secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    updateTimerDisplay(minutes, 0)
+
 
     if (minutes <= 0){
-      buttonStop.classList.add('hide')
-      buttonSet.classList.remove('hide')
-      buttonPause.classList.add('hide')
-      buttonPlay.classList.remove('hide')
+      resetControls()
       return
     }
 
     if( seconds <= 0) {
-      seconds = 2
-
-      minutesDisplay.textContent = String(minutes - 1).padStart(2, "0") /*padStart(2, "0") - preenchimento, nesse exemplo serão usados dois espaços e o primeiro será uma string "0"*/
+      seconds = 10
+      --minutes
     }
 
-    secondsDisplay.textContent = String(seconds - 1).padStart(2, "0")
+    updateTimerDisplay(minutes, String(seconds - 1))
 
-    cauntdow()
+    countdow()
   }, 1000) 
 
 }
 
-// callback - chamar afunção de volta
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonStop.classList.remove('hide')
   buttonSet.classList.add('hide')
 
-  cauntdow()
+  countdow()
   
 })
 
 buttonPause.addEventListener('click', function(){
   buttonPlay.classList.remove('hide')
   buttonPause.classList.add('hide')
+  clearTimeout(timerTimeOut) // espera que dentro dele tenha um ID numérico, diz para o JS para tudo
 
 })
 
 buttonStop.addEventListener('click', function() {
-  buttonStop.classList.add('hide')
-  buttonSet.classList.remove('hide')
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
-  
+  resetControls()
+  resetTimer()
 })
 
 buttonSet.addEventListener('click', function() {
@@ -72,8 +83,14 @@ buttonSet.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-  minutes = prompt('Quantos minutos você que adicionar?')
-  minutesDisplay.textContent = String(minutes).padStart(2, "0") /* textContent altera o conteúdo*/
+  let newMinutes = prompt('Quantos minutos você que adicionar?')
+  if (!newMinutes) {
+    resetTimer()
+    return
+  }
+
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
 })
 
 
