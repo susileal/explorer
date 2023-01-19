b641ca335f79afd76bab5a125cde0f142bc338dc
Susi Leal <susi_leal@hotmail.com>
2023-01-19 16:16:23 -0300

    delete

--- nivel6/github-favoritos - gitfav/js/favotites.js
+++ nivel6/github-favoritos - gitfav/js/favotites.js
@@ -4,29 +4,11 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
-  }
-}
-// classe que vai criar a visualização e eventos do html
-// FavoritesView - vai construir a visualização e extends (extender) a favorites
-//  super(root) é como se estivesse criando o link entre os dois
-
-// o this.root é a teg app - console.log(this.root)
-export class FavoritesView extends Favorites{
-  constructor(root) {
-    super(root)
-
-    this.update()
+    this.load()
   }
 
-  // construindo o html pelo js
-
-  update() {
-   this.removeAllTr()
-
-   // dados será um array contendo um objeto
-   // entries = entradas
-
-    const entries =[
+  load(){
+    this.entries = [
       {
         login: 'maykbrito',
         name: 'Mayk Brito',
@@ -42,9 +24,68 @@ export class FavoritesView extends Favorites{
       }
   
     ]
+  }
+
+  delete(user) {
+    // Higher-order functions (map, filter, find, reduce) - são funções de alta ordem
+    // filter - filtro. Vai rodar uma função para cada entrada
+    // filter - se alguma função retornar falso eu vou remover e deixar o array vazio
+    // filter - respeita oprincipio da imutabilidade
+    const filteredEntreis = this.entries
+    .filter(entry => entry.login !== user.login)
+  }
+}
+// classe que vai criar a visualização e eventos do html
+// FavoritesView - vai construir a visualização e extends (extender) a favorites
+//  super(root) é como se estivesse criando o link entre os dois
+
+// o this.root é a teg app - console.log(this.root)
+export class FavoritesView extends Favorites{
+  constructor(root) {
+    super(root)
+
+    this.tbody = this.root.querySelector('table tbody')
+
+    this.update()
+  }
+
+  // construindo o html pelo js
+
+  update() {
+   this.removeAllTr()
+
+   // dados será um array contendo um objeto
+   // entries = entradas
+
+  
     
-    entries.forEach (user => {
-      console.log(user)
+    this.entries.forEach (user => {
+      // cada vez que entrar vai pegar uma row e transformar em this.creatRow()
+
+      const row = this.createRow()
+
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      //onclinck é apentas para um evento
+      // confirm - é um boleano V ou F
+
+      row.querySelector('.remove').onclick = () => {
+        const isok = confirm('Tem certeza que deseja deletar essa linha?')
+        
+        if (isok) {
+          this.delete(user)
+        }
+      }
+
+
+      // append - elemento html criado com a DOM
+
+      this.tbody.append(row)
     })
   
   }
@@ -75,13 +116,15 @@ export class FavoritesView extends Favorites{
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
+    
 
     // está pegando todas as linhas
     // para cada um deles que você entrar - .forEach
-    tbody.querySelectorAll('tr').forEach((tr) => {
-      tr.remove()
-    })
+
+    this.tbody.querySelectorAll('tr')
+      .forEach((tr) => {
+        tr.remove()
+      })
   }
 }
 
