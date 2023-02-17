917480f7526ac61edd71080c242088fd47025606
Susi Leal <susi_leal@hotmail.com>
2023-02-17 16:30:48 -0300

    post

--- nivel8/aplicacaonode/src/server.js
+++ nivel8/aplicacaonode/src/server.js
@@ -1,37 +1,20 @@
-
-const { request } = require("express");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-// através do request que obtém informações que estão sendo enviadas para o api
-//  params - os parâmetros são obrigatórios
-// params - passa o endereço direto na rota
-// params - são utilizados para dados simples
-// para passar o parâmetro é necessário colocar /:
-// como recuperar a informação do parâmetro? request.params.id
-// pode ser usado vários parâmetros: /message/:id/:user
-// desestruturando ( const { id, user } = request.params) para não repetir: ${request.params.id}, ${request.params.user} 
-app.get("/message/:id/:user", (request, response) => {
+// o POST não pode ser visualizado pelo navegar, pois usa como padrão o get.
+// será usado o insomia para rodar os post - localhost:3333/users (enviando o response)
+// utilizando o método POST envia a requisição pelo corpo da requisição
+// o POST é utilizado, principalmente para cadastrar alguma coisa
+// body, como será feito o envio pelo corpo da requisição? JSON
+app.post("/users", (request, response) => {
+ 
+  const { name, email, password } = request.body;
 
-  const { id, user } = request.params
-
-  response.send(`
-  Mensagem ID: ${id}.
-  Nome do usuário: ${user}.
-  `);
-});
-
-// o fato dos  parâmetros não estarem na rota não impede que seja acessada a rota.
-// query - os parâmetros NÃO são obrigatórios
-// http://localhost:3333/users?page=5&limit=6
-app.get("/users", (request, response) => {
-  const { page, limit } = request.query;
-
-  response.send(`Página: ${page}. Mostrar: ${limit}`);
+  response.json({ name, email, password });
 });
 
 
 const PORT = 3333;
-
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
