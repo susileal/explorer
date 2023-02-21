2f82e30d8c6674948a637807a35d052500ced71a
Susi Leal <susi_leal@hotmail.com>
2023-02-21 12:02:47 -0300

    implementada a navegacao entre as paginas

--- nivel6/universoSPA/js/index.js
+++ nivel6/universoSPA/js/index.js
@@ -1,13 +1,55 @@
-import './router.js'
-// objeto literal
-    const routes = {
-      "/": "/pages/home.html",
-      "/universe": "/pages/universe.html",
-      "/exploration": "/pages/exploration.html",
-      404: "/pages/404.html",
+import {Router} from './router.js'
+
+    const router = new Router()
+    router.add("/", "/pages/home.html")
+    router.add("/universe", "/pages/universe.html")
+    router.add("/exploration", "/pages/exploration.html")
+    router.add(404, "/pages/404.html")
+
+
+    router.handle()
+
+    window.onpopstate = () => router.handle()
+    window.route = () => router.route()
+
+
+    /* Pilares da orientação a objetos
+    Herança:
+
+    Toda função dentro de uma classe é chamada de metodo
+    Todo e qualquer variável dentro de uma classe é chamada de atributo
+    Ex:
+
+    class Passaro {
+      voar() {
+        alert('voar')
+      }
+    }
+    
+    class Pato extends Passaro{
+      constructor(){
+        super()
+      }
     }
 
-    handle()
+    class Galinha extends Passaro {
+       constructor(){
+        super()
+      }
 
-    window.onpopstate = () => handle()
-    window.route = () => route()
+      // Polimofismo
+      voar() {
+        alert('nao voa tao bem')
+      }
+    }
+
+    const pato = new Pato()
+    pato.voar()
+
+    const galinha = new Galinha()
+    pato.galinha()
+    */
+
+
+ 
+
--- nivel6/universoSPA/js/router.js
+++ nivel6/universoSPA/js/router.js
@@ -1,33 +1,9 @@
-/* criando classe
-class Router {
-  // ao criar uma função dentro de uma classe não precisa usar a palavra chave function
-  
-  sayHello() {
-    alert("Hello")
-  }
-}
+export class Router {
+  // objeto vazio
+  routes = {}
 
-
-// usando a classe para criar um objeto
-// tira uma fotografia das propriedades e valores que está dentro da classe e joga para o objeto (router)
-// Um dos princípios da orientação a objetos é não precisar recriar código e sim fazer reuso deles.
-
-const router = new Router()
-const router1 = new Router()
-
-router.sayHello()
-router1.sayHello()
-
-// falso, apresar da classe ser a mesma a instância é outra.
-console.log(router === router1)
-
-*/
-
-class Router {
-
-  // routeName -  "/" - propriedade
-  // page - "/pages/home.html" - valor
   add(routeName, page){
+    // this.routes[routeName] = page - é um objeto com nome e valor
     this.routes[routeName] = page
   }
   route (event) {
@@ -37,8 +13,6 @@ class Router {
     
     window.history.pushState({}, "", event.target.href)
 
-    // toda vez que for fazer uma refer~encia dentro de uma classe é necessário usar a paravra 'this'
-    // this - este
     this.handle()
   }
 
