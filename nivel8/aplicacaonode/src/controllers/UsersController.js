5747ce9474fabb6e972c499efa622c02d258e382
Susi Leal <susi_leal@hotmail.com>
2023-02-23 12:09:47 -0300

    AppError e install express-async-errors

--- nivel8/aplicacaonode/src/controllers/UsersController.js
+++ nivel8/aplicacaonode/src/controllers/UsersController.js
@@ -1,3 +1,7 @@
+// importando o AppError
+
+const AppError = require("../utils/AppError");
+
 // srá usada a class, porque dentro dela permite ter várias funções
 // um controller poderá no máximo ter 5 métodos/função, se for mais será melhor criar outro controller
 /* a classe terá:
@@ -16,6 +20,12 @@ class UsersController {
   // é uma função, não precisa colocar a palavra reservada function na frente, pq a classe já sabe que é uma função
   create(request, response){
     const { name, email, password } = request.body;
+
+    // usar o AppError para exibir algum tipo de error
+
+    if(!name){
+      throw new AppError("Nome é obrigatório!");
+    }
 
     // devolvendo o status colde - status(201)
     response.status(201).json({ name, email, password });
--- nivel8/aplicacaonode/src/server.js
+++ nivel8/aplicacaonode/src/server.js
@@ -1,3 +1,12 @@
+// é no server que será feito o tratamento de exceções
+
+require("express-async-errors");
+
+// importando o AppError
+
+const AppError = require("./utils/AppError")
+
+const { request, response } = require("express");
 const express = require("express");
 
 const routes = require("./routes");
@@ -8,6 +17,27 @@ app.use(express.json());
 
 // acessar as rotas, fala para a aplicação usar as rotas que estão no routes
 app.use(routes);
+
+// capturando o error, requisição, a resposta e o next
+
+app.use((error, request, response, next) => {
+
+  // primeiro saber se o error é gerado pelo lado do cliente
+
+  if(error instanceof AppError){
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
